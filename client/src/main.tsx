f5823f468609c8a9fff3e311bfa94d6892da8124
Alan Christofer <alanchris@webxspark.com>
2025-02-01 11:57:54 +0530

    [config] tailwindcss v4 + shadcn work arounds

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './components/theme-provider.tsx'
+import ThemeSwitcher from './components/ui/theme-switcher.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='dark' storageKey='bgenie-theme'>
+      <App />
+      <ThemeSwitcher />
+    </ThemeProvider>
   </StrictMode>,
 )
